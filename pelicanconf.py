ed7b59d91969eebf9e5d18cf2543131a35e8edd5
Raja CSP Raman <raja.raman@syndigo.com>
2024-11-16 10:53:36 +0530

    Updates for 2024-11-16

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,8 +1,8 @@
 
 
-AUTHOR          = '<your name>'  # use your name
-SITENAME        = 'title' # use title
-GITHUB_USERNAME = 'tactlabs' # use your username
+AUTHOR          = 'Raja CSP RAman'  # use your name
+SITENAME        = 'Python and ML Notes' # use title
+GITHUB_USERNAME = 'rajasgs' # use your username
 
 
 # Blogroll
@@ -12,7 +12,7 @@ LINKS = (
 
 # Social widget
 SOCIAL = (
-    ("GitHub", "https://github.com/<yourusername>/"),
+    ("GitHub", "https://github.com/rajasgs/"),
     ("LinkedIn", "<linkedin handle>"),
 )
 
