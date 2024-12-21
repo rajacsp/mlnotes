0e23b31a14340ee5953b527eb2d4d494f49aa81f
Raja CSP Raman <raja.raman@syndigo.com>
2024-12-21 14:03:30 +0530

    Updates for 2024-12-21

--- notebooks/basics/pyutil.py
+++ notebooks/basics/pyutil.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import importlib.metadata
 
 def get_myenv():
     return os.environ.get('CONDA_DEFAULT_ENV')
@@ -12,3 +13,20 @@ def get_pyv():
 def get_local_pyinfo():
 
     return f"conda env: {os.environ.get('CONDA_DEFAULT_ENV')}; pyv: {sys.version}"
+
+def get_installed_version(lib_name):
+    try:
+        version = importlib.metadata.version(lib_name)
+        return f"{lib_name}=={version}"
+    except importlib.metadata.PackageNotFoundError:
+        return f"{lib_name} is not installed in the current environment."
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+def ps2(lib_names):
+    lib_list = lib_names.split()
+    results = """"""
+    for lib_name in lib_list:
+        version = get_installed_version(lib_name)
+        results += f"{version}\n"
+    return results
