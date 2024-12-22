41cf52d874b7ec1097112a98004d5672729b1ca9
Raja CSP Raman <raja.raman@syndigo.com>
2024-12-22 10:34:30 +0530

    Updates for 2024-12-22

--- notebooks/zzemp/pyutil.py
+++ notebooks/zzemp/pyutil.py
@@ -12,3 +12,22 @@ def get_pyv():
 def get_local_pyinfo():
 
     return f"conda env: {os.environ.get('CONDA_DEFAULT_ENV')}; pyv: {sys.version}"
+
+import importlib.metadata
+
+def get_installed_version(lib_name):
+    try:
+        version = importlib.metadata.version(lib_name)
+        return f"{lib_name}=={version}"
+    except importlib.metadata.PackageNotFoundError:
+        return f"{lib_name} is not installed in the current environment."
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+def ps2(lib_names):
+    lib_list = lib_names.split()
+    results = """"""
+    for lib_name in lib_list:
+        version = get_installed_version(lib_name)
+        results += f"{version}\n"
+    return results
